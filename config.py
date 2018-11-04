bf84a10e582e82c1e66c46b55bb8c08f3a9aeb57
Adam Bieńkowski <donadigos159@gmail.com>
2018-11-04 23:03:44 +0100

    Fix config.py pointing to the wrong train data file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 NET_CONFIG = {
     'batch_size': 128,
     'lookback': 20,
-    'train_fname': 'data/train_data2.pkl',
+    'train_fname': 'data/train_data.pkl',
     'patterns_fname': 'data/pattern_data.pkl'
 }
